29617e104437d3acdcacd8cd8044a45cb18d5dbb
Ongera <ongeradaryn@gmail.com>
2026-06-18 02:26:48 +0300

    fix: Changed the default from {} to {}

--- priv/repo/migrations/20260617221511_create_session_checkpoints.exs
+++ priv/repo/migrations/20260617221511_create_session_checkpoints.exs
@@ -4,7 +4,7 @@ defmodule Jiyi.Repo.Migrations.CreateSessionCheckpoints do
   def change do
     create table(:session_checkpoints, primary_key: false) do
       add(:session_id, :text, primary_key: true, null: false)
-      add(:working_memory, :jsonb, null: false, default: "'{}'")
+      add(:working_memory, :jsonb, null: false, default: "{}")
       add(:updated_at, :timestamptz, null: false)
     end
   end
